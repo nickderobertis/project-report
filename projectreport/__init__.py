045f3a7d047ab34d6a12a1ad1644c29cf43263ad
Nick DeRobertis <whoopnip@gmail.com>
2020-03-29 16:34:29 -0400

    Add commonly used functionality to __init__.py

--- projectreport/__init__.py
+++ projectreport/__init__.py
@@ -2,3 +2,9 @@
 A set of tools for describing software projects. Finds software projects, analyzes them,
 and outputs reports.
 """
+from projectreport.analyzer.project import Project
+from projectreport.finder.python import PythonPackageFinder
+from projectreport.finder.git import GitFinder
+from projectreport.finder.combine import CombinedFinder
+from projectreport.report.report import Report
+from projectreport.config import DEFAULT_IGNORE_PATHS
