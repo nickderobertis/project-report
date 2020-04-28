99d03bced5c0f267e31d8da8ce120ff9a82a3743
Nick DeRobertis <whoopnip@gmail.com>
2020-04-28 04:20:48 -0400

    Additional restructuring and retry for requests and github exceptions in monkey patch of pygithub

--- projectreport/tools/monkey_patch_github.py
+++ projectreport/tools/monkey_patch_github.py
@@ -1,49 +1,91 @@
 from datetime import timezone, datetime, timedelta
 import time
 import types
-from typing import Union
+from typing import Union, Optional, List
 
-from github import RateLimitExceededException
+from github import RateLimitExceededException, GithubException
 from github import Github
 from github.GithubObject import GithubObject
 from github.RateLimit import RateLimit
 from github.Rate import Rate
 from github.Requester import Requester
+from requests.exceptions import RequestException
 
 from projectreport.logger import logger
 
 RATE_LIMIT_PAD_SECONDS = 180
+REQUEST_EXCEPTION_WAIT = 60
 
 
 def monkey_patch_github_obj_for_throttling(gh_obj: Union[Github, GithubObject]):
     requester = _get_requester(gh_obj)
     orig_request_and_check = requester.requestJsonAndCheck
 
-    def request_json_and_check_patched_for_throttling(obj: Requester, verb: str, url: str, **kwargs):
+    def request_json_and_check_patched_for_throttling(obj: Requester, verb: str, url: str,
+                                                      retries_remaining: int = 3,
+                                                      collected_exceptions: Optional[List[Exception]] = None,
+                                                      **kwargs):
+        if retries_remaining <= 0:
+            raise GithubException(
+                f'could not make github request after retries. Got errors {collected_exceptions}',
+                {'exc': collected_exceptions}
+            )
+        if collected_exceptions is None:
+            collected_exceptions = []
+
         logger.debug(f'made {verb} request to {url}')
         try:
             return orig_request_and_check(verb, url, **kwargs)
-        except RateLimitExceededException:
-            headers, data = obj.requestJsonAndCheck("GET", "/rate_limit")
-            limits = RateLimit(obj, headers, data["resources"], True)
-            reset = _get_last_reset_datetime_utc(limits)
-            now = datetime.now(timezone.utc)
-            diff = reset - now
-            seconds = diff.total_seconds()
-            current_tz = datetime.utcnow().astimezone().tzinfo
-            logger.info(f"Rate limit exceeded")
-            logger.info(f"Reset is at {reset.astimezone(current_tz)}.")
-            if seconds > 0.0:
-                # Extra 10s to ensure it's ready
-                logger.info(f"Waiting for {diff + timedelta(seconds=RATE_LIMIT_PAD_SECONDS)}")
-                time.sleep(seconds + RATE_LIMIT_PAD_SECONDS)
-                logger.info(f"Done waiting - resuming at {datetime.now()}")
-            return orig_request_and_check(verb, url, **kwargs)
+        except RateLimitExceededException as e:
+            collected_exceptions.append(e)
+            _get_rate_limit_and_wait(requester)
+            return request_json_and_check_patched_for_throttling(
+                obj,
+                verb,
+                url,
+                retries_remaining=retries_remaining - 1,
+                collected_exceptions=collected_exceptions,
+                **kwargs
+            )
+        except (RequestException, GithubException) as e:
+            collected_exceptions.append(e)
+            _wait_for_read_timeout(e)
+            return request_json_and_check_patched_for_throttling(
+                obj,
+                verb,
+                url,
+                retries_remaining=retries_remaining - 1,
+                collected_exceptions=collected_exceptions,
+                **kwargs
+            )
 
     requester.requestJsonAndCheck = types.MethodType(
         request_json_and_check_patched_for_throttling,
         requester
     )
+
+
+def _wait_for_read_timeout(e: RequestException):
+    logger.warning(f'Got request/Github exception: {e}. Sleeping for {REQUEST_EXCEPTION_WAIT}s')
+    time.sleep(REQUEST_EXCEPTION_WAIT)
+    logger.info(f'Resuming from read timeout sleep at {datetime.now()}')
+
+
+def _get_rate_limit_and_wait(obj: Requester):
+    headers, data = obj.requestJsonAndCheck("GET", "/rate_limit")
+    limits = RateLimit(obj, headers, data["resources"], True)
+    reset = _get_last_reset_datetime_utc(limits)
+    now = datetime.now(timezone.utc)
+    diff = reset - now
+    seconds = diff.total_seconds()
+    current_tz = datetime.utcnow().astimezone().tzinfo
+    logger.info(f"Rate limit exceeded")
+    logger.info(f"Reset is at {reset.astimezone(current_tz)}.")
+    if seconds > 0.0:
+        # Extra 10s to ensure it's ready
+        logger.info(f"Waiting for {diff + timedelta(seconds=RATE_LIMIT_PAD_SECONDS)}")
+        time.sleep(seconds + RATE_LIMIT_PAD_SECONDS)
+        logger.info(f"Done waiting - resuming at {datetime.now()}")
 
 
 def _get_last_reset_datetime_utc(limit: RateLimit) -> datetime:
