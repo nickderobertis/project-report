6844e559d880f64e66beb3bbb7c8e670b25c7e72
Nick DeRobertis <whoopnip@gmail.com>
2022-06-05 12:24:50 -0400

    chore: Try using shutil.rmtree with ignore_errors=True to get windows working rather than cleanup with fallback

--- tests/dirutils.py
+++ tests/dirutils.py
@@ -1,4 +1,5 @@
 import contextlib
+import shutil
 import tempfile
 from pathlib import Path
 from typing import Iterator
@@ -14,13 +15,9 @@ def create_temp_path() -> Iterator[Path]:
     2. On MacOS, the temp directory has a symlink. This resolves the symlink so that
        there won't be any mismatch in resolved paths.
     3. On Windows, the temp directory can fail to delete with a PermissionError. This function
-       will try to delete the temp directory, but if it fails with PermissionError or OSError
-       it will just ignore it.
+       will try to delete the temp directory, but if it fails with an error it will just ignore it.
     """
     temp_dir = tempfile.TemporaryDirectory()
     temp_path = Path(temp_dir.name).resolve()
     yield temp_path
-    try:
-        temp_dir.cleanup()
-    except (PermissionError, OSError):
-        pass
+    shutil.rmtree(temp_path, ignore_errors=True)
