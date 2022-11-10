964e108f708e9c98b458ea6f36e848e48d4c7b20
Nick DeRobertis <whoopnip@gmail.com>
2022-11-10 18:25:32 -0500

    feat: Add more default ignore paths

--- projectreport/config.py
+++ projectreport/config.py
@@ -2,5 +2,8 @@ import os
 
 DEFAULT_IGNORE_PATHS = (
     ".*",  # hidden files and folders
-    "__pycache__",  # python cache
+    "__pycache__",  # python cache,
+    "venv",  # virtual environment
+    "venvs",
+    "node_modules",  # JS dependencies
 )
